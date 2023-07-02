76088af20ef9fc971d2492cc624069760be84b5c
THIAGO SANTOS DOS ANJOS <thiago_santos_1990@hotmail.com>
2023-07-01 21:06:11 -0300

    Add files via upload

---
+++ abstraindo_celular_POO_C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+
+Smartphone nokia = new Nokia(numero: "984212491", modelo: "Modelo 1", imei: "123456789", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("TikTok");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartpone iPhone: ");
+Smartphone iphone = new Iphone(numero: "983039538", modelo: "Modelo 2", imei: "987654321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhasApp");
